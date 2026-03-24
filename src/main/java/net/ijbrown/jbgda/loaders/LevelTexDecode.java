6502d074cfd412c33aa1b75fa232eaca6d1533ea
bigianb <bigianb@gmail.com>
2026-03-24 00:02:09 +0000

    vq

--- src/main/java/net/ijbrown/jbgda/loaders/LevelTexDecode.java
+++ src/main/java/net/ijbrown/jbgda/loaders/LevelTexDecode.java
@@ -142,6 +142,7 @@ public class LevelTexDecode
         }
         public int lenBytes;
         public int subtracts;
+        public int numBits;
         public List<Integer> values;
     }
 
@@ -151,10 +152,12 @@ public class LevelTexDecode
 
         int pos = offset;
         int numBits = DataUtil.getLEInt(fileData, pos);
+        table.numBits = numBits;
         table.subtracts = 0x21 - numBits;
 
-        int code2 =  DataUtil.getLEInt(fileData, pos + 8);
         pos += 4;
+        int code2 =  DataUtil.getLEInt(fileData, pos + 4);
+
         while (code2 != -1) {
             int code1 = DataUtil.getLEInt(fileData, pos);
             numBits += 1;
@@ -162,9 +165,12 @@ public class LevelTexDecode
             table.values.add(code1 >> 2);
 
             code2 =  DataUtil.getLEInt(fileData, pos + 4);
-            pos += 8;
+            if (code2 != -1) {
+                // so we can calculate len bytes.
+                pos += 8;
+            }
         }
-
+        table.lenBytes = pos + 8 - offset;
 
         return table;
     }
@@ -185,9 +191,318 @@ public class LevelTexDecode
 
         // offset to first huffman table
         int off1 = palOffset + v1 * 4;
-        decodeHuffman(off1);
+        var table1 = decodeHuffman(off1);
+
+        int off2 = off1 + table1.lenBytes;
+
+        var table2 = decodeHuffman(off2);
+
+        int off3 = off2 + table2.lenBytes;
 
 
+        int blocksWidth = 1;
+        int bw = (pixelWidth + 0xf) >> 4;
+        if (bw > 1) {
+            do {
+                blocksWidth += 1;
+            } while (1 << (blocksWidth & 0x1f) <= bw);
+        }
+
+        int blocksHeight = 1;
+        int bh = (pixelHeight + 0xf) >> 4;
+        if (bh > 1) {
+            do {
+                blocksHeight += 1;
+            } while (1 << (blocksHeight & 0x1f) <= bh);
+        }
+
+        Logger.info("pixel width = {}, bw = {}, pixel height = {}, bh = {}", pixelWidth, bw, pixelHeight, bh);
+
+        int compressedData2 = compressedDataOffset + 4;
+
+        int iStack_94 = 0x40 - blocksHeight;
+        int iStack_98 = 0x40 - blocksWidth;
+
+        long uStack_1e0 = 0;
+        int iStack_1d8 = 0;
+        while( true ) {
+            long x0 = 0;
+            long uVar34 = uStack_1e0;
+            // read blocksWidth bits from stream into iStack9c
+            if (blocksWidth != 0) {
+                for (; iStack_1d8 < blocksWidth; iStack_1d8 += 8) {
+                    long bVar2 = fileData[compressedData2];
+                    int xx = 0x38 - iStack_1d8;
+                    compressedData2++;
+                    uVar34 |= bVar2 << xx;
+                }
+                uStack_1e0 = uVar34 << blocksWidth;
+                iStack_1d8 -= blocksWidth;
+                x0 = (uVar34 >>> iStack_98);
+            }
+            long y0 = 0;
+            uVar34 = uStack_1e0;
+            // read blocksHeight bits into y0
+            if (blocksHeight != 0) {
+                for (; iStack_1d8 < blocksHeight; iStack_1d8 += 8) {
+                    long bVar2 = fileData[compressedData2];
+                    y0 = 0x38 - iStack_1d8;
+                    compressedData2++;
+                    uVar34 |= bVar2 << y0;
+                }
+                uStack_1e0 = uVar34 << blocksHeight;
+                iStack_1d8 -= blocksHeight;
+                y0 = (uVar34 >>>  iStack_94);
+            }
+            long x1 = 0;
+            uVar34 = uStack_1e0;
+            // read blocksWidth bits into x1
+            if (blocksWidth != 0) {
+                for (; iStack_1d8 < blocksWidth; iStack_1d8 += 8) {
+                    long bVar2 = fileData[compressedData2];
+                    int yy = 0x38 - iStack_1d8;
+                    compressedData2++;
+                    uVar34 |= bVar2 << yy;
+                }
+                uStack_1e0 = uVar34 << blocksWidth;
+                iStack_1d8 -= blocksWidth;
+                x1 = (uVar34 >>> iStack_98);
+            }
+            long y1 = 0;
+            uVar34 = uStack_1e0;
+            // read blocksHeight bits into y1
+            if (blocksHeight != 0) {
+                for (; iStack_1d8 < (int) blocksHeight; iStack_1d8 += 8) {
+                    long bVar2 = fileData[compressedData2];
+                    int shift = 0x38 - iStack_1d8;
+                    compressedData2++;
+                    uVar34 |= bVar2 << shift;
+                }
+                uStack_1e0 = uVar34 << blocksHeight;
+                iStack_1d8 -= blocksHeight;
+                y1 = (uVar34 >>>  iStack_94);
+            }
+            if (x1 < x0) break;
+
+            long y = y0;
+
+            if (y0 < y1){
+                do {
+                    long x = x0;
+                    if (x0 < x1){
+                        long pixel_y = y * 0x10;
+                        int iStack_64 = 0;
+                        long pixel_y_bot = pixel_y + 0x10;
+                        //pcStack_6c = texturePageTable + pageNum;
+                        do {
+                            //puVar40 = &uStack_1e0;
+                            y0 = 0;
+                            long iStack_74 = x * 0x10;
+                            int sVar3 = pixelWidth;
+                            long lVar18 = iStack_74 + 0x10;
+                            if (sVar3 <= lVar18){
+                                lVar18 = sVar3;
+                            }
+                            uVar34 = uStack_1e0;
+                            var lVar44 = pixel_y_bot;
+                            if (pixelHeight <= pixel_y_bot) {
+                                lVar44 = pixelHeight;
+                            }
+                            // read 1 bit from the stream
+                            for (; iStack_1d8 < 1; iStack_1d8 += 8) {
+                                long bVar2 = fileData[compressedData2];
+                                int shift = 0x38 - iStack_1d8;
+                                compressedData2++;
+                                uVar34 |= bVar2 << shift;
+                            }
+                            var uVar16 = uVar34 << 1;
+                            iStack_1d8 += -1;
+
+                            uStack_1e0 = uVar16;
+
+                            if (uVar34 < 0) {
+                                // Bit read is 1
+                                // Read 24 bits
+                                for (; iStack_1d8 < 0x18; iStack_1d8 += 8) {
+                                    long bVar2 = fileData[compressedData2];
+                                    compressedData2++;
+                                    uVar16 |= bVar2 << (0x38 - iStack_1d8);
+                                }
+                                uStack_1e0 = uVar16 << 0x18;
+                                iStack_1d8 = iStack_1d8 + -0x18;
+
+                                var uVar43 = (uVar16 >>> 0x20);
+                                uVar34 = (uVar43 >> 8) & 7;
+                                //cVar1 = *pcStack_6c;
+                                uVar43 >>= 8;
+                                var palX = uVar43 >> 3;
+                                /*
+                                if (cVar1 < 9) {
+                                    pbStack_b4 = (byte *)(*(int *)(world + 0x7c) +
+                                            (uint)(byte)(&texturePageSegLoc)
+                                                      [((int)uVar43 >> 0x14) + cVar1 * 0x80] *
+                                    0x20000 + (palX & 0x1ffff));
+                                }
+                                else {
+                                    pbStack_b4 = (byte *)((int)&texturePageLoc[cVar1] + palX);
+                                }
+                                */
+                                //puVar40 = &uStack_1d0;
+                                //uStack_b8 = 0;
+                                //uStack_c0 = 0;
+                                //uStack_1c8._0_4_ = 0;
+                                //uStack_1d0 = 0;
+                                //uStack_1c8._4_4_ = pbStack_b4;
+                                /*
+                                if (uVar34 != 0) {
+                                    do {
+                                        bVar2 = *uStack_1c8._4_4_;
+                                        uStack_1c8._4_4_ = uStack_1c8._4_4_ + 1;
+                                        palY = 0x38 - (int)uStack_1c8;
+                                        uStack_1c8 += 8;
+                                        uStack_1d0 |= (ulong)bVar2 << (long)palY;
+                                    } while ((long)(int)uStack_1c8 < (long)uVar34);
+
+                                    uStack_1d0 <<= uVar34;
+                                    uStack_1c8 -= uVar34;
+                                }
+                                for (; uStack_1c8 < 1; uStack_1c8 += 8) {
+                                    uStack_1d0 |= (ulong)*uStack_1c8._4_4_ << (long)(0x38 - (int)uStack_1c8);
+                                    uStack_1c8._4_4_ = uStack_1c8._4_4_ + 1;
+                                }
+                                uStack_1d0 = uStack_1d0 << 1;
+                                uStack_1c8 = CONCAT44(uStack_1c8._4_4_,(int)uStack_1c8 + -1);
+
+                                 */
+                            }
+                            var lVar42 = pixel_y;
+
+                            /*
+                            if (*puStack_a8 == 0) {
+                                for (; lVar42 < lVar44; lVar42 = (long)((int)lVar42 + 2)) {
+                                    palY = y0 * 0x20;
+                                    iVar19 = palY + 0x10;
+                                    y0 += 1;
+                                    for (iVar37 = iStack_74; iVar37 < lVar18; iVar37 += 2) {
+                                        puVar39 = auStack_1c0 + iVar19;
+                                        auStack_1c0[palY] = huffmanTemp;
+                                        iVar20 = palY + 1;
+                                        iVar7 = iVar19 + 1;
+                                        palY += 2;
+                                        auStack_1c0[iVar20] = DAT_00355d01;
+                                        iVar19 += 2;
+                                        *puVar39 = DAT_00355d02;
+                                        auStack_1c0[iVar7] = DAT_00355d03;
+                                    }
+                                }
+                            } else {
+                                for (; lVar42 < lVar44; lVar42 = (long)((int)lVar42 + 2)) {
+                                    lVar45 = (long)iStack_74;
+                                    palY = y0 * 0x20;
+                                    iVar19 = palY + 0x10;
+                                    y0 += 1;
+                                    if (lVar45 < lVar18) {
+                                        do {
+                                            if ((int)puVar40[1] < 0x20) {
+                                                do {
+                                                    pbVar15 = *(byte **)((int)puVar40 + 0xc);
+                                                    uVar34 = puVar40[1];
+                                                    bVar2 = *pbVar15;
+                                                    iVar37 = (int)uVar34 + 8;
+                                                    *(byte **)((int)puVar40 + 0xc) = pbVar15 + 1;
+                                                    *(int *)(puVar40 + 1) = iVar37;
+                                                    *puVar40 = *puVar40 | (ulong)bVar2 << (long)(0x38 - (int)uVar34);
+                                                } while (iVar37 < 0x20);
+                                                iVar37 = (int)puVar40[1];
+                                            } else {
+                                                iVar37 = (int)puVar40[1];
+                                            }
+                                            uVar34 = *puVar40;
+                                            *(int *)(puVar40 + 1) = iVar37 + -0x20;
+                                            uVar43 = (uint)(uVar34 >> 0x20);
+                                            lVar25 = (long)(int)(uVar43 >> 2);
+                                            iVar20 = 0;
+                                            auVar26 = _pextlw(lVar25,lVar25);
+                                            auVar26 = _pextlw(auVar26._0_8_,auVar26._0_8_);
+                                            puVar38 = &DAT_0037dd90;
+                                            uVar28 = gbits;
+                                            uVar30 = DAT_0037dd84;
+                                            uVar31 = DAT_0037dd88;
+                                            uVar33 = DAT_0037dd8c;
+                                            do {
+                                                auVar10._4_4_ = uVar30;
+                                                auVar10._0_4_ = uVar28;
+                                                auVar10._8_4_ = uVar31;
+                                                auVar10._12_4_ = uVar33;
+                                                auVar36 = _pcgtw(auVar10,auVar26);
+                                                auVar36 = _ppach(in_zero_qw,auVar36);
+                                                auVar36 = _ppacb(in_zero_qw,auVar36);
+                                                uVar28 = *puVar38;
+                                                uVar30 = puVar38[1];
+                                                uVar31 = puVar38[2];
+                                                uVar33 = puVar38[3];
+                                                uVar35 = _plzcw(auVar36._0_8_ >> 1);
+                                                puVar38 = puVar38 + 4;
+                                                palX = (int)uVar35 + 1U >> 3;
+                                                iVar20 += palX;
+                                            } while (-1 < (int)(palX - 4));
+                                            palX = gsubtracts - iVar20;
+                                            *puVar40 = uVar34 << 0x20;
+                                            iVar20 = (&gsubtracts)[palX];
+                                            if ((long)(int)palX != 0) {
+                                                *(uint *)(puVar40 + 1) = iVar37 + -0x20 + palX;
+                                                *puVar40 = (uVar34 << 0x20) >> (long)(int)palX |
+                                                        ((long)uVar34 >> 0x20 & ~(long)(-1 << (palX & 0x1f)) & 0xffffffff) << (long)(int)(0x40 - palX);
+                                            }
+                                            iVar20 = ((uVar43 >> (palX & 0x1f)) - iVar20) * 4;
+                                            auStack_1c0[palY] = (&huffmanTemp)[iVar20];
+                                            puVar39 = auStack_1c0 + iVar19;
+                                            lVar45 = (long)((int)lVar45 + 2);
+                                            auStack_1c0[palY + 1] = (&DAT_00355d01)[iVar20];
+                                            iVar37 = iVar19 + 1;
+                                            palY += 2;
+                                            iVar19 += 2;
+                                            *puVar39 = (&DAT_00355d02)[iVar20];
+                                            auStack_1c0[iVar37] = (&DAT_00355d03)[iVar20];
+                                        } while (lVar45 < lVar18);
+                                    }
+                                }
+                            }
+                            */
+
+                            /*
+                            y0 = 0;
+                            iVar19 = 0;
+                            palY = 0;
+                            do {
+                                iVar37 = 7;
+                                puVar39 = (undefined1 *)((iStack_64 * 8 + palY) * 4 + (int)puStack_78);
+                                do {
+                                    pbVar15 = &unscrambleTable + y0;
+                                    pbVar22 = &DAT_006e9081 + y0;
+                                    iVar37 += -1;
+                                    pbVar24 = &DAT_006e9082 + y0;
+                                    pbVar29 = &DAT_006e9083 + y0;
+                                    y0 += 4;
+                                    *puVar39 = auStack_1c0[*pbVar15];
+                                    puVar39[1] = auStack_1c0[*pbVar22];
+                                    puVar39[2] = auStack_1c0[*pbVar24];
+                                    puVar39[3] = auStack_1c0[*pbVar29];
+                                    puVar39 = puVar39 + 4;
+                                } while (-1 < iVar37);
+                                iVar19 += 1;
+                                palY += iStack_84;
+                            } while (iVar19 < 8);
+                            */
+
+                            x++;
+                            iStack_64++;
+                        } while (x < x1);
+                    }
+                    ++y;
+                } while (y < y1);
+            }
+        }
     }
 
     public void extract(File outputfile, int offset, int chunkStartOffset) throws IOException
